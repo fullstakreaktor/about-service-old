c614f098262c7c2442361f742f99da3ac69b75e5
Joyce Cho <joyjoycho@gmail.com>
2018-07-20 21:00:39 -0700

    mockdata-hosts

---
+++ db/mockdata.js
@@ -0,0 +1,30 @@
+const features = ['market', 'hiking', 'shopping mall', 'castro', 'snowboarding', 'dog park', 'historic'];
+
+const thingsToDo = ['Stores, coffee shops, restaurants, and pubs are great here.', 'chinatown is a must go', 'Windy Ocean beach', 'welcome to foggy city'];
+
+const locations = [[-17.8336363, 48.4088007], [32.993182, 110.425983], [50.7480989, 20.2540479]];
+
+const hostGuidebooks = ['https://www.airbnb.com/things-to-do/rooms/2736358', 'https://www.airbnb.com/things-to-do/rooms/1345358', 'https://www.airbnb.com/things-to-do/rooms/1345663'];
+
+const firstNames = ['John', 'Sam', 'Bentley', 'Nova', 'Lily', 'Tofu', 'Snow']
+
+const lastNames = ['Cho', 'Pham', 'Johnson', 'Smith'];
+
+const cities = ['Ćuprija', 'Bacnotan', 'Henggang', 'Erba', 'Salitrillos', 'Avanca', 'Stuttgart', 'Tišina', 'Caujul', 'Esperança', 'Santa Cruz', 'Växjö', 'Rabak', 'Atlanta', 'Cimrutu', 'Prażmów', 'Mancha Khiri', 'Luohuang', 'Yuanping'];
+
+
+const states = ['New York', 'Missouri', 'California', 'Ohio', 'Alabama', 'Louisiana', 'Alabama', 'Missouri', 'Pennsylvania', 'Texas'];
+
+const countries = ['Peru', 'Brazil', 'South Korea', 'Russia', 'Turkmenistan', 'Argentina', 'Argentina', 'Egypt', 'Malaysia', 'Sweden'];
+
+const joinInDates = ['2015-02-05', '2018-05-26', '2011-02-16', '2015-02-13', '2016-12-16', '2015-06-26', '2015-02-06', '2013-08-21', '2016-05-18', '2011-10-07'];
+
+const descriptions = ['Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.'];
+
+const emails = ['dlippitt0@chicagotribune.com', 'dcharlwood1@icio.us', 'lboichat2@tinyurl.com', 'nbuckle3@gov.uk', 'gtestro4@unblog.fr', 'gwapples5@ameblo.jp', 'jgallichiccio6@howstuffworks.com', 'vvasyushkhin7@squidoo.com', 'ameggison8@xrea.com', 'aglentz9@yolasite.com'];
+
+const urls = ['www.photobooth.com/url1']
+
+module.exports = {
+  features, thingsToDo, locations, hostGuidebooks, firstNames, lastNames, cities, states, countries, joinInDates, descriptions, emails, urls,
+};
