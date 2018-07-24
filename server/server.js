cb2663e85b92abaee6d36bdb0e5ecb9368fbb0ea
Joyce Cho <joyjoycho@gmail.com>
2018-07-24 12:06:44 -0700

    add query function that for server get request

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+// need to create a file to select data
+// const db = require('../db/')
 
 const app = express();
+
+// to parse our data and use req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('hello world');
