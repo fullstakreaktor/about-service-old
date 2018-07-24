dbf3dfb2786c9e920e761469644492f0ac2c74ef
Joyce Cho <joyjoycho@gmail.com>
2018-07-23 23:37:42 -0700

    get server the render to 3001

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3002;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+app.get('/', (req, res) => {
+  res.send('hello world');
 });
+
+app.listen(3001, () => {
+  console.log('Server started on 3001');
+});
+
+
+// const express = require('express');
+// const morgan = require('morgan');
+// const path = require('path');
+// const app = express();
+// const port = process.env.PORT || 3002;
+//
+// app.use(morgan('dev'));
+// app.use(express.static(path.join(__dirname, '../public')));
+//
+// app.listen(port, () => {
+//   console.log(`server running at: http://localhost:${port}`);
+// });
