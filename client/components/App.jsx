f1b2322c67ac518dc52fa2d36dd8fbb589bae032
Joyce Cho <joyjoycho@gmail.com>
2018-07-25 01:51:53 -0700

    display host basic information with basic css

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
+// import styles from './styles.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,20 +8,30 @@ class App extends React.Component {
 
     this.state = {
       host: {},
+      joinMonth: '',
+      joinYear: '',
     };
   }
 
   componentWillMount() {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
     $.get('/hosts', (data) => {
       this.setState({ host: data[0] });
-      console.log(this.state.host.first_name);
+      this.setState({joinMonth: monthNames[Number(this.state.host.joined_in_date.split('-')[1]) - 1] });
+      this.setState({joinYear: this.state.host.joined_in_date.split('-')[0]});
     });
   }
 
   render() {
     return (
       <div>
-        <span>Hosted By {this.state.host.first_name}</span>
+        <span className='title'>Hosted By {this.state.host.first_name}</span>
+        <div>
+          <span>{this.state.host.city}, {this.state.host.state}, {this.state.host.country} · Joined in {this.state.joinMonth}, {this.state.joinYear}
+          </span>
+        </div>
       </div>
     );
   }
