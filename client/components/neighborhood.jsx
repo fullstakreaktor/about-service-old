9c4fcda346ed4099fd455788cd6803eb54ff0fe3
Joyce Cho <joyjoycho@gmail.com>
2018-07-28 01:49:29 -0700

    get map rendered on the page

--- client/components/neighborhood.jsx
+++ client/components/neighborhood.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import GoogleMap from './Map.jsx';
 import styles from './css/hostInfo.css';
 import CSSModules from 'react-css-modules';
 
 const Neighborhood = (props) => {
-  console.log(props);
+  let location = {lat: props.lat, lng: props.lng};
+
   return (
     <div styleName='infoBox'>
       <p styleName="title">
@@ -17,6 +19,7 @@ const Neighborhood = (props) => {
         <span styleName='boldingWords'>Things to do · </span>
         <span>{props.neighborhoodInfo.things_to_do}</span>
       </p>
+      <GoogleMap location={location} zoom='11' apiKey={props.apiKey}/>
     </div>
   );
 };
